4111b303fd2d766d553b68a69606d596dc33b3cc
mo-alsafi <alsafimo12@gmail.com>
2024-12-11 20:52:02 +0200

    pub

--- server.js
+++ server.js
@@ -1,21 +1,15 @@
 import express from 'express';
-import { json } from 'express';
-import router from './routes/index.js'; // Importing routes
+import controllerRouting from './routes/index';
 
-// Create the express app
 const app = express();
-
-// Middleware to parse JSON
-app.use(json());
-
-// Use the routes from routes/index.js
-app.use(router);
-
-// Get the port from the environment variable or default to 5000
 const port = process.env.PORT || 5000;
 
-// Start the server
+app.use(express.json());
+
+controllerRouting(app);
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
 
+export default app;
